9a8db7ab46e54c45322ef5ae6f10d1c1a8258f1f
nanodev94 <nanodev94@gmail.com>
2025-05-22 13:36:59 +0200

    added locale selector to header

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,49 +1,61 @@
 'use client'
 
+import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
 import Image from 'next/image'
-import { useTranslations } from 'next-intl'
+import { useLocale, useTranslations } from 'next-intl'
+import { zodResolver } from '@hookform/resolvers/zod'
+import clsx from 'clsx'
 
 import Button from '@/components/Button'
+import Select from '@/components/Select'
+import type { SelectOption } from '@/components/Select/Select'
 import { PAGE } from '@/constants'
 import { useAppDispatch, useAppSelector } from '@/globalHooks/redux'
-import { Link } from '@/i18n/navigation'
+import { Link, usePathname, useRouter } from '@/i18n/navigation'
+import { SUPPORTED_LOCALES } from '@/i18n/routing'
 import { reset, selectUser } from '@/redux/slices/userSlice'
 
-/*
-const LOCALE_OPTIONS: SelectOption[] = [
-  { value: 'es', label: 'ES' },
-  { value: 'en', label: 'EN' },
-]
-*/
+import { type HeaderData, headerSchema } from './zodSchema'
+
+const LOCALE_OPTIONS: SelectOption[] = SUPPORTED_LOCALES.map((locale) => ({
+  value: locale,
+  label: locale.toUpperCase(),
+}))
 
 const Header = () => {
   const t = useTranslations('header')
   const dispatch = useAppDispatch()
-  // const locale = useLocale()
-  // const router = useRouter()
-  // const pathname = usePathname()
+  const locale = useLocale()
+  const router = useRouter()
+  const pathname = usePathname()
 
   // TODO: check close session button when user has session and reload page
   const userData = useAppSelector(selectUser)
+
+  const { control, watch } = useForm<HeaderData>({
+    resolver: zodResolver(headerSchema),
+    defaultValues: {
+      locale: LOCALE_OPTIONS.find((option) => option.value === locale),
+    },
+  })
+
+  const currentLocale = watch('locale')
 
   const sections = [
     { href: PAGE.SEARCH, label: t('sections.search') },
     { href: PAGE.SETS, label: t('sections.sets') },
   ]
 
-  /*
-  const localeDefaultValue = LOCALE_OPTIONS.find(
-    (option) => option.value === locale
-  )
-
-  const handleLocaleChange = (newValue: SingleValue<SelectOption>) => {
-    router.replace({ pathname }, { locale: newValue?.value })
-  }
-  */
-
   const handleCloseSession = () => {
     dispatch(reset())
   }
+
+  useEffect(() => {
+    if (currentLocale.value !== locale) {
+      router.replace({ pathname }, { locale: currentLocale.value })
+    }
+  }, [currentLocale])
 
   return (
     <header className='bg-header p-4 flex items-center justify-between gap-4 sticky top-0 z-50'>
@@ -51,7 +63,10 @@ const Header = () => {
         <Image alt='App Icon' height={40} src='/favicon.webp' width={40} />
         {sections.map(({ href, label }) => (
           <Link
-            className='text-xl font-bold hover:text-red-500 transition-all duration-300'
+            className={clsx(
+              'text-xl font-bold hover:text-red-500 transition-all duration-300',
+              pathname === href && 'text-red-500'
+            )}
             href={href}
             key={label}
           >
@@ -60,14 +75,7 @@ const Header = () => {
         ))}
       </div>
       <div className='flex items-center justify-end gap-4 grow'>
-        {/* <Search /> */}
-        {/*
-        <Select
-          defaultValue={localeDefaultValue}
-          onChange={handleLocaleChange}
-          options={LOCALE_OPTIONS}
-        />
-        */}
+        <Select control={control} name='locale' options={LOCALE_OPTIONS} />
         {userData ? (
           <Button onClick={handleCloseSession} rounded>
             {t('closeSession')}
